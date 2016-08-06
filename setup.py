eb1b94a9d5fe97ba70259c248b4bc4e0129f6691
Calin Vlad <vlad.s.calin@gmail.com>
2016-08-06 17:12:42 +0300

    added travis.yml and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="pymockdata",
+    version="0.0.1",
+    description="A Python library for generating mock data "
+                "and exporting in various formats or directly into a database",
+    author="Vlad Calin",
+    author_email="vlad.s.calin@gmail.com",
+    license="MIT",
+    url="https://github.com/vladcalin/pymockdata",
+    packages=["pymockdata"],
+    entry_points={
+        "console_scripts": [
+            "pymockdata = pymockdata.cli:main"
+        ]
+    },
+    test_suite="tests",
+    install_requires=[
+        "click>=5.0.0"
+    ]
+)
