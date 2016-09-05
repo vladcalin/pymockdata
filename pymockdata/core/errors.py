f3314207414c1ac565c02debbd0fd95d7525e224
vladcalin <vlad.s.calin@gmail.com>
2016-09-05 13:30:48 +0300

    started rewrite

--- pymockdata/core/errors.py
+++
@@ -1,4 +0,0 @@
-
-
-class PyMockDataError(Exception):
-    pass
--- pymockdata/data/internet.py
+++
@@ -1,4 +0,0 @@
-tld = [
-    ".com", ".net", ".org", ".io", ".co", ".ro", ".md", ".ru", ".es", ".info", ".us", ".de", ".jp", ".biz"
-]
-
